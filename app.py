1e73f06d705124cae61bc2835ce88cbf6c9accc3
Nimish Kumar <nimishkumar0305@gmail.com>
2024-03-27 00:07:00 +0530

    Frontend + DB created

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for, redirect, request, session
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'BloodLink_DBMS123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bloodbank.db'
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True,port=5500)
